e3d1218d9fe96f47aa888bb23dcfb50e23a3b3f6
dronni3de <dronni3de@gmail.com>
2017-03-26 13:09:30 +0300

    Fix for images and video bgd

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,20 @@
 $( document ).ready(function() {
 
 
-    
+    // var BV = new $.BigVideo({
+    //     useFlashForFirefox:true,
+    //     forceAutoplay:false,
+    //     controls:false,
+    //     doLoop:false,
+    //     container:$('.hero-1'),
+    //     shrinkable:false
+    // });
+    //
+    // BV.init();
+    //
+    // BV.show(['../../video/iStock-466238634.mp4','../../video/iStock-466238634.mp4','../../video/iStock-466238634.mp4'],{ambient:true});
+
+
+
+
 });
